7a9c2495563314c6bf17688a0590188aaafa4215
enrihg <99612570+enrihg@users.noreply.github.com>
2024-02-14 18:36:00 -0300

    basic behaviour added for slideshow button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,45 @@
 import logo from '../../assets/shared/logo.svg';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useRef } from 'react';
+import data from '../../data.js';
 
 function Navbar() {
+
+    const [id, setId] = useState(1); //asdfsdfsdsdfsf
+    const ref = useRef(1);
+    const navigate = useNavigate();
+
+    let interval;
+
+    const startSlideshow = () => {
+        interval = setInterval(() => {
+            ref.current === data.length ? stopSlideshow() : ref.current = ref.current + 1;
+            console.log(ref.current);
+            // setId(ref.current);
+            navigate(`/${ref.current}`)
+        },
+            3000)
+    }
+
+    const stopSlideshow = () => {
+        clearInterval(interval)
+        ref.current = 1;
+    }
+
+
+
     return (
+
         <nav className='Navbar'>
             <Link to='/'>
                 <h1><img className='logo' src={logo} alt="logo" /></h1>
             </Link>
-            <button>START SLIDESHOW</button>
+
+            <Link to={`/${ref.current}`}>
+                <button onClick={startSlideshow}>START SLIDESHOW</button>
+            </Link>
+
         </nav>
     )
 }
