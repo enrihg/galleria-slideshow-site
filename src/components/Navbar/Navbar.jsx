74cec8b100e501437ae78bfb2a31a1e71bafb0b7
enrihg <99612570+enrihg@users.noreply.github.com>
2024-02-16 10:26:37 -0300

    responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,45 +1,14 @@
 import logo from '../../assets/shared/logo.svg';
 import './Navbar.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useState, useRef } from 'react';
-import data from '../../data.js';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 
-    const [id, setId] = useState(1); //asdfsdfsdsdfsf
-    const ref = useRef(1);
-    const navigate = useNavigate();
-
-    let interval;
-
-    const startSlideshow = () => {
-        interval = setInterval(() => {
-            ref.current === data.length ? stopSlideshow() : ref.current = ref.current + 1;
-            console.log(ref.current);
-            // setId(ref.current);
-            navigate(`/${ref.current}`)
-        },
-            3000)
-    }
-
-    const stopSlideshow = () => {
-        clearInterval(interval)
-        ref.current = 1;
-    }
-
-
-
     return (
-
         <nav className='Navbar'>
             <Link to='/'>
                 <h1><img className='logo' src={logo} alt="logo" /></h1>
             </Link>
-
-            <Link to={`/${ref.current}`}>
-                <button onClick={startSlideshow}>START SLIDESHOW</button>
-            </Link>
-
         </nav>
     )
 }
